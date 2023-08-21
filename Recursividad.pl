c5c3132cf8f0d4951b9ee8354f291ecefa60a019
Nestor Recinos <00083120@UCA.EDU.SV>
2023-08-21 17:06:38 -0600

    :sparkles: Add factorial exercise

--- Recursividad.pl
+++ Recursividad.pl
@@ -1,5 +1,8 @@
 % Conteo ascendente partiendo de 1
-contar(X, N):-
+contarN(N):-
+  contarN(1, N).
+
+contarN(X, N):-
   =:=(X, N),
   writeln(X),
   !.
@@ -9,3 +12,19 @@ contarN(X, N):-
   writeln(X),
   is(Xsig, +(X,1)),
   contarN(Xsig, N).
+
+% Factorial
+factorial(N, Ac):-
+  =:=(N,0),
+  writeln(1),
+  !.
+
+factorial(N, Ac):-
+  =:=(N,1),
+  writeln(Ac),
+  !.
+
+factorial(N, Ac):-
+  is(AcSig, *(N, Ac)),
+  is(Nant, -(N, 1)),
+  factorial(Nant, AcSig).
