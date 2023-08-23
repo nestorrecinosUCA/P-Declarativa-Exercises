71980dc9a52a180e6defca4f948240295fe814be
Nestor Recinos <00083120@UCA.EDU.SV>
2023-08-23 16:18:10 -0600

    :sparkles: Add exercises about recursivity

--- Recursividad.pl
+++ Recursividad.pl
@@ -28,3 +28,31 @@ factorial(N, Ac):-
   is(AcSig, *(N, Ac)),
   is(Nant, -(N, 1)),
   factorial(Nant, AcSig).
+
+% Calcular el n-ésimo término de Fibonacci:
+fibo(1, 0):- !.
+fibo(2, 1):- !.
+fibo(N, Fn):-
+  is(Nm1, -(N, 1)),
+  is(Nm2, -(N, 2)),
+  fibo(Nm1, Fn1),
+  fibo(Nm2, Fn2),
+  is(Fn, +(Fn1, Fn2)).
+
+% Sumar todos los enteros comprendidos en el intervalo [a,b]
+sumar_intervalo(A, B, _):-
+  >(A, B),
+  writeln('Escribe los valores al revés'),
+  !,
+  fail.
+
+sumar_intervalo(A, B, Suma):- 
+  =:=(A, B),
+  is(Suma, B),
+  !.
+
+sumar_intervalo(A, B, Suma):-
+  <(A, B),
+  is(Asig, +(A, 1)),
+  sumar_intervalo(Asig, B, SumaTemp),
+  is(Suma, +(SumaTemp, A)).
