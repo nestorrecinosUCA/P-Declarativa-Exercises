be6747b01828058c3bbe74d2ab7e29afdcfbb0d7
Nestor Recinos <00083120@UCA.EDU.SV>
2023-08-27 18:03:01 -0600

    :sparkles: Add siblings and uncles rules

--- arbol_familiar.pl
+++ arbol_familiar.pl
@@ -1,40 +1,58 @@
 % Madres
-mother(josefina, marisol).
+mother(josefina, mario).
 mother(josefina, ligia).
 mother(silvia, marina).
 mother(silvia, jaime).
 mother(silvia, carlos).
 mother(ligia, regina).
 mother(ligia, jacinta).
+mother(marlene, julio).
 
 % Padres
-father(manuel, marisol).
+father(manuel, mario).
 father(manuel, ligia).
 father(mario, marina).
 father(mario, jaime).
 father(mario, carlos).
 father(julio, regina).
 father(julio, jacinta).
+father(irvin, julio).
 
 % Abuelo:
-grandfather(X,Y) :-
+grandfather(X, Y) :-
   father(X, W),
-  father(W,Y).
+  father(W, Y).
 grandfather(X,Y):-
   father(X, W),
-  mother(W,Y).
+  mother(W, Y).
 
 % Abuela:
-grandmother(X,Y):-
+grandmother(X, Y):-
   mother(X, W),
   father(W,Y).
 grandmother(X,Y):-
   mother(X, W),
-  mother(W,Y).
+  mother(W, Y).
 
-% Abuelas
+% Abuelos
 grandparents(X,Y) :- grandfather(X,Y).
+% Abuelas
 grandparents(X,Y) :- grandmother(X,Y).
 
+% hermanos:
+sibling(X, Y):-
+  father(P, X),
+  father(P, Y),
+  mother(M, X),
+  mother(M, Y),
+  X \= Y.
 
 % HACER CLAUSULAS PARA PRIMOS, HERMANOS, TIOS, ETC
+uncles(X, Y):-
+  father(P, Y),
+  sibling(X, P).
+
+uncles(X, Y):-
+  mother(M, Y),
+  sibling(X, M).
+  %father(P, Y),
